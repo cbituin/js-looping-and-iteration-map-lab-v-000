f53847238b979c98089ac2c945df6f8f66cedbf8
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-09-19 19:24:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const lowerCaseDrivers = (arr) => {
   return arr.map(val => val.toLowerCase());
 };
 
-const nameToAttributes = () => {
+const nameToAttributes = (arr) => {
   return arr.map(val =>
     arr.firstname = val.split(" ")[0],
     arr.lastname = val.split(" ")[1]
